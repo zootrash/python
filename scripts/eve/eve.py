8462be5c1a0c9c42ed2b81cdf4ed0efddecaa337
zootrash <zootrash@users.noreply.github.com>
2016-10-08 18:04:21 -0700

    converting eve from sh to py

---
+++ scripts/eve/eve.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#
+# Eve: The personal and mysterious AI
+#
+####################
+#
+# eve pre-alpha
+#
+####################
+
+# libraries
+import time		# time of OS
+
+# vars
+c_time = time.strftime("Today's date is %I:%M:%S")
+print (c_time)
