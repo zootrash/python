4b93bcf9fd80358c15ec0a14607a8e361a82efa2
zootrash <zootrash@users.noreply.github.com>
2016-10-09 17:59:54 -0700

    upd eve!

--- scripts/eve/eve.py
+++ scripts/eve/eve.py
@@ -11,13 +11,34 @@
 # libraries
 import time		# time of OS
 
-# get user input
-userinput = input('Yes?\n\n')
+# loop counter
+hello = 1
 
 # date
-__DATE__ = time.strftime("Today's date is %B %d, %Y")
-print(__DATE__)
+__DATE__ = time.strftime("Today's date is %B %d, %Y.")
 
 # time
-__TIME__ = time.strftime("The time is currently %I:%M %p")
-print(__TIME__)
+__TIME__ = time.strftime("The time is currently %I:%M %p.")
+
+
+# eve loop
+print('Yes?')
+while(hello):
+
+	userinput = input('\n')
+
+	if "no" in userinput:
+		print('Farewell.')
+		hello = 0
+
+	if(hello == 1):
+		# testing user input
+		if "date" in userinput:
+			print(__DATE__)
+		elif "time" in userinput:
+			print(__TIME__)
+		else:
+			print('I am unable to reply to your comment.')
+
+		print('Anything else?')
+
