e22104901488ed6f45e3b9ceea9710e2d62d7ebf
zootrash <zootrash@users.noreply.github.com>
2016-11-07 14:52:40 -0800

    messin with eve

--- scripts/eve/eve.py
+++ scripts/eve/eve.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 #
 # Eve: The personal and mysterious AI
 #
@@ -20,22 +20,30 @@ __DATE__ = time.strftime("Today's date is %B %d, %Y.")
 # time
 __TIME__ = time.strftime("The time is currently %I:%M %p.")
 
+def goodbye(userInput)
+	"Farewell Eve"
+	if "no" in userInput
+		print('Farewell.')
+		return 0
+
+def respond(userInput):
+	"Respond to user input"
+
+	goodbye(userInput)
+
 
 # eve loop
 print('Yes?')
 while(hello):
 
-	userinput = input('\n')
-
-	if "no" in userinput:
-		print('Farewell.')
-		hello = 0
+	userInput = input('\n')
+	hello = respond(userInput)
 
 	if(hello == 1):
 		# testing user input
-		if "date" in userinput:
+		if "date" in userInput:
 			print(__DATE__)
-		elif "time" in userinput:
+		elif "time" in userInput:
 			print(__TIME__)
 		else:
 			print('I am unable to reply to your comment.')
